88d45df62d7aa6895860690f106e03f93f55d818
Max Goldman <maxg@mit.edu>
2013-06-19 11:32:01 -0400

    Labels on y-axis

--- day8-visual/visual.js
+++ day8-visual/visual.js
@@ -1,27 +1,49 @@
-var data = [ 0, 4, 8, 8, 15, 16, 23, 42 ];
+var data = [ 4, 8, 8, 15, 16, 23, 42 ];
 
-var chart_width = 300;
-var chart_height = 300;
+var outer_width = 300;
+var outer_height = 300;
+var margin = { top: 20, right: 20, bottom: 20, left: 20 };
+var chart_width = outer_width - margin.left - margin.right;
+var chart_height = outer_height - margin.top - margin.bottom;
 
 var x_scale = d3.scale.ordinal().domain(d3.keys(data)) .rangeBands([0, chart_width]);
-var y_scale = d3.scale.linear().domain([0, d3.max(data)]).range([0, chart_height]);
+var y_scale = d3.scale.linear().domain([0, d3.max(data)]).range([chart_height, 0]);
 
-var chart = d3.select(".chart-container").append("svg")
-  .attr("class", "chart")
-  .attr("height", chart_height)
-  .attr("width", chart_width);
+var chart = d3.select(".chart-container")
+  .append("svg")
+    .attr("class", "chart").attr("height", outer_height).attr("width", outer_width)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+chart.selectAll("line").data(y_scale.ticks(10))
+  .enter().append("line")
+    .attr("x1", 0)
+    .attr("x2", chart_width)
+    .attr("y1", y_scale)
+    .attr("y2", y_scale);
+
+chart.selectAll(".y-scale-label").data(y_scale.ticks(10))
+  .enter().append("text")
+    .attr("class", "y-scale-label")
+    .attr("x", 0)
+    .attr("y", y_scale)
+    .attr("dx", -margin.left/8)
+    .attr("dy", "0.3em")
+    .attr("text-anchor", "end")
+    .text(String);
 
 chart.selectAll("rect").data(data)
   .enter().append("rect")
     .attr("x", function(d, i) { return x_scale(i); })
-    .attr("y", function(d) { return chart_height - y_scale(d); })
+    .attr("y", y_scale)
     .attr("width", x_scale.rangeBand())
-    .attr("height", y_scale);
+    .attr("height", function(d) { return chart_height - y_scale(d); });
 
-chart.selectAll("text").data(data)
+chart.selectAll(".bar-label").data(data)
   .enter().append("text")
+    .attr("class", "bar-label")
     .attr("x", function(d, i) { return x_scale(i) + x_scale.rangeBand()/2; })
-    .attr("y", function(d) { return chart_height - y_scale(d) + 3; })
+    .attr("y", function(d) { return y_scale(d) + margin.top/4; })
     .attr("dy", "0.7em")
     .attr("text-anchor", "middle")
     .text(function (d) { return d; });
