095e96e3fc5f346fe90d82ed2aa2642d692c0f76
Max Goldman <maxg@mit.edu>
2013-06-19 10:34:28 -0400

    Switched to using SVG

--- day8-visual/visual.js
+++ day8-visual/visual.js
@@ -1,12 +1,24 @@
 var data = [ 0, 4, 8, 8, 15, 16, 23, 42 ];
 
-var x_scale = d3.scale.linear()
-                .domain([0, d3.max(data)]).range(["0%", "100%"]);
+var chart_height = 140;
 
-var chart = d3.select(".chart-container")
-                .append("div")
-                .attr("class", "chart");
-chart.selectAll("div").data(data)
-  .enter().append("div")
-    .style("width", x_scale)
-    .text(function(d) { return d; });
+var x_scale = d3.scale.linear().domain([0, d3.max(data)]).range(["0%", "100%"]);
+var y_scale = d3.scale.ordinal().domain(d3.keys(data))
+                  .rangeBands([0, chart_height]);
+
+var chart = d3.select(".chart-container").append("svg").attr("class", "chart");
+
+chart.selectAll("rect").data(data)
+  .enter().append("rect")
+    .attr("y", function(d, i) { return y_scale(i); })
+    .attr("width", x_scale)
+    .attr("height", 20);
+
+chart.selectAll("text").data(data)
+  .enter().append("text")
+    .attr("x", x_scale)
+    .attr("y", function(d, i) { return y_scale(i) + y_scale.rangeBand()/2; })
+    .attr("dx", -3)
+    .attr("dy", "0.35em")
+    .attr("text-anchor", "end")
+    .text(function (d) { return d; })
